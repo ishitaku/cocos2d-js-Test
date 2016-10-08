e9c23bf4c91b71273e2bdae36d861cbc7994c7d2
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-08 18:36:16 +0900

    a

--- js/title.js
+++ js/title.js
@@ -12,7 +12,25 @@ var gamestart = cc.Layer.extend({
         	var backgroundLayer = cc.Sprite.create(res.gigu_png);
 		backgroundLayer.setPosition(size.width/2,size.height /2 );
         	this.addChild(backgroundLayer);
-	}
+        	
+        	// タップイベントリスナーを登録する
+                cc.eventManager.addListener({
+                    event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                    swallowTouches: true,
+                    onTouchBegan: this.onTouchBegan,
+                    onTouchMoved: this.onTouchMoved,
+                    onTouchEnded: this.onTouchEnded
+                }, this);
+	},
+	onTouchBegan: function(touch, event) {
+        return true;
+      },
+      onTouchMoved: function(touch, event) {},
+      onTouchEnded: function(touch, event) {
+        // 次のシーンに切り替える
+        cc.audioEngine.stopMusic();
+        cc.director.runScene(new gameScene());
+      },
 	
 });
 

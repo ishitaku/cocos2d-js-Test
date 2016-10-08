ea8d1f83300416ac4a684bb5b7f656997232c6a8
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-08 18:04:05 +0900

    a

--- js/title.js
+++ js/title.js
@@ -4,11 +4,12 @@ var gamestart = cc.Layer.extend({
 	ctor: function() {
 		this._super();
 		var size = cc.director.getWinSize();
-		var backgroundLayer = cc.Sprite.create(res.gigu_png);
+        	
+        	var backgroundLayer = cc.Sprite.create(res.background_png);
 		backgroundLayer.setPosition(size.width/2,size.height /2 );
         	this.addChild(backgroundLayer);
         	
-        	var backgroundLayer = cc.Sprite.create(res.background_png);
+        	var backgroundLayer = cc.Sprite.create(res.gigu_png);
 		backgroundLayer.setPosition(size.width/2,size.height /2 );
         	this.addChild(backgroundLayer);
 	}

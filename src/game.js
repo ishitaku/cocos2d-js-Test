74781931d9d0c5d695fc636f1089b76e9612e469
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-10 13:19:01 +0900

    a

--- src/game.js
+++ src/game.js
@@ -428,7 +428,7 @@ var Shrimp = cc.Sprite.extend({
        life--;
        lifeText.setString("LIFE : " + life);
        if(life < 1){
-         audioEngine.stopMusic();
+         //audioEngine.stopMusic();
          gameover.score = score;
          cc.director.runScene(new gameover());
        }
